d3ce08a01dde33e4e7ad01a99100d385f1fb2ee6
uha (mac) <xkj@gmx.de>
2018-06-18 11:14:06 +0200

    bla2

--- testPyScaffold/sfTrident/sftrident/converter.py
+++ testPyScaffold/sfTrident/sftrident/converter.py
@@ -5,7 +5,31 @@ converts constants
 import math as m
 
 def alpha2charge(alpha):
+    """Converts finestructure constant to charge
+
+    Parameters
+    ----------
+    alpha : float
+        value of finestructure constant
+
+    Returns
+    -------
+    float
+        value of charge
+    """
     return m.sqrt(4.0*m.pi*alpha)
 
 def charge2alpha(charge):
+    """Converts charge to finestructure constant
+
+    Parameters
+    ----------
+    charge : float
+        value of charge
+
+    Returns
+    -------
+    float
+        value of finestructure constant
+    """
     return charge**2/(4.0*m.pi)
--- testPyScaffold/sfTrident/sftrident/current.py
+++ testPyScaffold/sfTrident/sftrident/current.py
@@ -19,6 +19,21 @@ gamma=qft.GammaMatrix()
 
 
 class currentClass(object):
+    """Calculation of the four currents
+
+    Parameters
+    ----------
+    config : dict
+        The configuration dictionary which contains at least intensity parameter 'a0', mass of the particles 'mass' and polarisation parameter 'xi'.
+
+    Notes
+    -----
+    The currents to be calculated are:
+
+    .. math:: J^\mu_0 = u_A(p_A) \gamma^\mu v_B(p_B)
+
+    where
+    """
     def __init__(self,config=None):
         self.a0=config['a0']
         self.mass = config['mass']
@@ -29,6 +44,9 @@ class currentClass(object):
         self.cleanSet()
 
     def cleanSet(self):
+        """
+        utility function to set general output
+        """
         self.J0 =None# np.array([qft.MinkowskiVector([0,0,0,0]) for t in range(4)])
         self.J1 =None# np.array([qft.MinkowskiVector([0,0,0,0]) for t in range(4)])
         self.J2 =None# np.array([qft.MinkowskiVector([0,0,0,0]) for t in range(4)])
@@ -43,6 +61,19 @@ class currentClass(object):
     def generalJ0(self,spinorA,spinorB):
         """
         calc of J0 for general spinors
+
+        Parameters
+        ----------
+        spinorA : np.ndarray
+            array of qft.spinorUbar of particle A (for both spins)
+
+        spinorB : np.ndarray
+            array of qft.spinorUbar of particle B (for both spins)
+
+        Returns
+        -------
+        np.ndarray
+            array of four qft.MinkowskiVector (each spin combination)
         """
         current1 = np.zeros(4,dtype=np.complex)
         current2 = np.zeros(4,dtype=np.complex)
@@ -60,7 +91,31 @@ class currentClass(object):
         """
         calc of J1/2 for general spinors and momenta
 
-        pol ... mks.Vector for background (general) polarisation
+        Parameters
+        ----------
+        momPhoto : qft.MinkowskiVector
+            momentum of the initial (background) photon
+
+        momA : qft.MinkowskiVector
+            momentum of the particle A
+
+        momB : qft.MinkowskiVector
+            momentum of the particle B
+
+        spinorA : np.ndarray
+            array of qft.spinorUbar of particle A (for both spins)
+
+        spinorB : np.ndarray
+            array of qft.spinorUbar of particle B (for both spins)
+
+        pol : qft.MinkowskiVector
+            polarisation vector of the background field
+
+        Returns
+        -------
+        np.ndarray
+            array of four qft.MinkowskiVector (each spin combination)
+
         """
         current1 = np.zeros(4,dtype=np.complex)
         current2 = np.zeros(4,dtype=np.complex)
@@ -80,6 +135,28 @@ class currentClass(object):
     def generalJ3(self,momPhoto,momA,momB,spinorA,spinorB):
         """
         calc of J3 for general spinors and momenta
+
+        Parameters
+        ----------
+        momPhoto : qft.MinkowskiVector
+            momentum of the initial (background) photon
+
+        momA : qft.MinkowskiVector
+            momentum of the particle A
+
+        momB : qft.MinkowskiVector
+            momentum of the particle B
+
+        spinorA : np.ndarray
+            array of qft.spinorUbar of particle A (for both spins)
+
+        spinorB : np.ndarray
+            array of qft.spinorUbar of particle B (for both spins)
+
+        Returns
+        -------
+        np.ndarray
+            array of four qft.MinkowskiVector (each spin combination)
         """
         prefac=-self.a0**2*self.mass**2/2.0
         diracMatrix = prefac * qft.feyndagg(momPhoto)/(momPhoto*momB)/(momPhoto*momA)
@@ -90,6 +167,31 @@ class currentClass(object):
         return np.array([momPhoto*current1,momPhoto*current2,momPhoto*current3,momPhoto*current4])
 
     def setKin(self,momPhoto,momA,momB,spinorA,spinorB):
+        """
+        sets the kinematics for the whole instance and computes all currents
+
+        Parameters
+        ----------
+        momPhoto : qft.MinkowskiVector
+            momentum of the initial (background) photon
+
+        momA : qft.MinkowskiVector
+            momentum of the particle A
+
+        momB : qft.MinkowskiVector
+            momentum of the particle B
+
+        spinorA : np.ndarray
+            array of qft.spinorUbar of particle A (for both spins)
+
+        spinorB : np.ndarray
+            array of qft.spinorUbar of particle B (for both spins)
+
+        Notes
+        -----
+        After this method call, one may use the currents directly from the instance, e.g. <currentInstance>.J0 returns J0.
+
+        """
         self.momPhoto = momPhoto
         self.momA = momA
         self.momB = momB
@@ -101,17 +203,89 @@ class currentClass(object):
         self.evalJ3()
 
     def evalJ0(self):
+        """
+        Evaluation of the current J0.
+
+        Parameters
+        ----------
+        None
+            parameters given by setKin()
+
+        Returns
+        -------
+        np.ndarray
+            array of four qft.MinkowskiVector (each spin combination)
+
+        Notes
+        -----
+        Requires setting of kinemtatics:
+            <currentInstance>.setKin()
+        """
         self.J0 = self.generalJ0(self.spinorA,self.spinorB)
         return self.J0
 
     def evalJ1(self):
+        """
+        Evaluation of the current J1.
+
+        Parameters
+        ----------
+        None
+            parameters given by setKin()
+
+        Returns
+        -------
+        np.ndarray
+            array of four qft.MinkowskiVector (each spin combination)
+
+        Notes
+        -----
+        Requires setting of kinemtatics:
+            <currentInstance>.setKin()
+        """
         self.J1 = self.generalJ12(self.momPhoto,self.momA,self.momB,self.spinorA,self.spinorB,self.polBase[0])
         return self.J1
 
     def evalJ2(self):
+        """
+        Evaluation of the current J2.
+
+        Parameters
+        ----------
+        None
+            parameters given by setKin()
+
+        Returns
+        -------
+        np.ndarray
+            array of four qft.MinkowskiVector (each spin combination)
+
+        Notes
+        -----
+        Requires setting of kinemtatics:
+            <currentInstance>.setKin()
+        """
         self.J2 = self.generalJ12(self.momPhoto,self.momA,self.momB,self.spinorA,self.spinorB,self.polBase[1])
         return self.J2
 
     def evalJ3(self):
+        """
+        Evaluation of the current J3.
+
+        Parameters
+        ----------
+        None
+            parameters given by setKin()
+
+        Returns
+        -------
+        np.ndarray
+            array of four qft.MinkowskiVector (each spin combination)
+
+        Notes
+        -----
+        Requires setting of kinemtatics:
+            <currentInstance>.setKin()
+        """
         self.J3 = self.generalJ3(self.momPhoto,self.momA,self.momB,self.spinorA,self.spinorB)
         return self.J3
