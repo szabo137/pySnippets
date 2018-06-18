3f4358ce280f2fdd604314f5a0c10338ede8172c
uha (hzdr:fwklux5) <u.hernandez@hzdr.de>
2018-06-18 11:15:44 +0200

    bla

--- testPyScaffold/sfTrident/docs/conf.py
+++ testPyScaffold/sfTrident/docs/conf.py
@@ -87,8 +87,8 @@ release = ''  # Is set by calling `setup.py docs`
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-#exclude_patterns = ['_build','api/sftrident.qft.rst']
-exclude_patterns = ['_build']
+exclude_patterns = ['_build','api/sftrident.qft.rst']
+#exclude_patterns = ['_build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 # default_role = None
